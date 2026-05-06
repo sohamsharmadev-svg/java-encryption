6f93373e109462db1def02d2624476091eb73dcd
Soham Sharma <sohamsharma.dev@gmail.com>
2026-05-06 18:19:00 +0530

    Initial commit - Java Encryption Program

---
+++ src/encryptionprogram/Main.java
@@ -0,0 +1,7 @@
+package encryptionprogram;
+
+public class Main {
+    public static void main(String[] args) {
+        new EncryptionProgram();
+    }
+}
---
+++ src/encryptionprogram/module-info.java
@@ -0,0 +1,2 @@
+module encryptionprogram {
+}
